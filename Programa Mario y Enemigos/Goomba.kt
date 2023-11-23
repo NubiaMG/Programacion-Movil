a094b329755abcce54f6990d41d27dc61fea2b64
NubiaMG <144622972+NubiaMG@users.noreply.github.com>
2023-11-22 19:12:30 -0600

    Update Goomba.kt

--- Programa Mario y Enemigos/Goomba.kt
+++ Programa Mario y Enemigos/Goomba.kt
@@ -1,9 +1,7 @@
+//Esta es la clase goomba
 package Clases
 
 class Goomba: Enemy("Goomba", 1){
-    //(name: String, strength: Int):
-    //Enemy(name,strength) {
-    //Enemy("Goomba",1) {
     init {
         println("Iniciando subclase de $name")
     }
